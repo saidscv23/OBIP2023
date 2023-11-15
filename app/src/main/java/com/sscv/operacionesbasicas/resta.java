84f3419ace437e6e8612924386d9d34b6813bddb
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-11-15 16:43:35 -0500

    Creacion de las ctivity y un Layout por cada operación básica

---
+++ app/src/main/java/com/sscv/operacionesbasicas/resta.java
@@ -0,0 +1,14 @@
+package com.sscv.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resta);
+    }
+}
