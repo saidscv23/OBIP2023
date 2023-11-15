3718879cbe77dfddf880e84400c319f15340285a
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-11-15 17:37:44 -0500

    Lógica de la Multiplicacion implementada en la actividad de Android.

--- app/src/main/java/com/sscv/operacionesbasicas/multiplicacion.java
+++ app/src/main/java/com/sscv/operacionesbasicas/multiplicacion.java
@@ -3,12 +3,29 @@ package com.sscv.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class multiplicacion extends AppCompatActivity {
-
+    EditText edNumM1,edNumM2;
+    TextView tvResultadoM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplicacion);
+        edNumM1=findViewById(R.id.numM);
+        edNumM2=findViewById(R.id.numM2);
+        tvResultadoM=findViewById(R.id.lblresultadoM);
+    }
+    public void calcularMultiplicacion(View v){
+
+        double num1=Double.parseDouble(edNumM1.getText().toString());
+        int num2=Integer.parseInt(edNumM2.getText().toString());
+        double resultado=num1*num2;
+        tvResultadoM.setText(resultado+"");
+
+
+
     }
 }
