13e7546c1f06f21f15295c4b8e163e99707b5553
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-11-15 10:57:14 -0500

    Evento click con la suma de dos numeros

--- app/src/main/java/com/sscv/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/sscv/operacionesbasicas/MainActivity.java
@@ -3,12 +3,33 @@ package com.sscv.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNum1,edNum2;
+    TextView tvResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edNum1=findViewById(R.id.num1);
+        edNum2=findViewById(R.id.num2);
+        tvResultado=findViewById(R.id.lblresultado);
+    }
+
+    public void calcularSuma(View v){
+
+        double num1=Double.parseDouble(edNum1.getText().toString());
+        int num2=Integer.parseInt(edNum2.getText().toString());
+        double resultado=num1+num2;
+        tvResultado.setText(resultado+"");
+
+
+
     }
 }
