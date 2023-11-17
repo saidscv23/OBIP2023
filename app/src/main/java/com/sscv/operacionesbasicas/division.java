f0fbaa78af06067d8535393cfc63ae49e3aca5ed
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-11-17 09:10:09 -0500

    Creacion de la funcionalidad de Divicion

--- app/src/main/java/com/sscv/operacionesbasicas/division.java
+++ app/src/main/java/com/sscv/operacionesbasicas/division.java
@@ -9,22 +9,22 @@ import android.widget.TextView;
 
 public class division extends AppCompatActivity {
     EditText edNumD1,edNumD2;
-    TextView tvResultadoR;
+    TextView tvResultadoD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_division);
         edNumD1=findViewById(R.id.numD);
         edNumD2=findViewById(R.id.numD2);
-        tvResultadoR=findViewById(R.id.lblresultadoD);
+        tvResultadoD=findViewById(R.id.lblresultadoD);
     }
 
     public void calcularDivision(View v){
 
         double num1=Double.parseDouble(edNumD1.getText().toString());
         int num2=Integer.parseInt(edNumD2.getText().toString());
-        double resultado=num1-num2;
-        tvResultadoR.setText(resultado+"");
+        double resultado=num1/num2;
+        tvResultadoD.setText(resultado+"");
 
 
 
